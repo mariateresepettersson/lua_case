ef41d2057639e88d23671376152b4fd42ab3bd4c
ettpunktnoll-01 <39116969+ettpunktnoll-01@users.noreply.github.com>
2024-02-19 10:14:43 +0100

    Create lua_case.lua

---
+++ lua_case.lua
@@ -0,0 +1,38 @@
+local input = {
+    greeting = {
+        eng = "Hello World!",
+        swe = "Hallå Världen!",
+        ger = "Hallo Welt!",
+        lorem = "Lorem ipsum!",
+        text_id = 1
+    },
+    headline = {
+        eng = "This is a headline.",
+        swe = "Det här en rubrik.",
+        ger = "Dies ist eine Überschrift.",
+        lorem = "Dolor sit amet.",
+        text_id = 2
+    },
+    content = {
+        eng = "This is the main content. It consists of two sentences.",
+        swe = "Det här är huvudinnehållet. Det består av två meningar.",
+        ger = "Dies ist der Hauptinhalt. Er besteht aus zwei Sätzen.",
+        lorem = "Quisque at luctus libero. Lorem ipsum dolor sit amet.",
+        text_id = 3
+    },
+    footer = {
+        eng = "Last updated: February 15 2024.",
+        swe = "Senast uppdaterad: 15 februari 2024.",
+        ger = "Zuletzt aktualisiert: 15. Februar 2024.",
+        lorem = "Donec iaculis facilisis: Jan 1 1970.",
+        text_id = 4
+    }
+}
+
+local lang_id = "swe"
+
+--[[
+
+Your code here.
+
+]]
